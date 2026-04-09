63b37825a55f92d3ac6fb7b7cc2cc12754050c18
LoosePrince <pfingan@foxmail.com>
2026-04-09 13:36:33 +0800

    优化快捷键注册逻辑，修正语言文件中的键分类标识符。

--- src/client/java/com/portablestorage/PortableStorageClient.java
+++ src/client/java/com/portablestorage/PortableStorageClient.java
@@ -14,6 +14,7 @@ import com.portablestorage.upgrade.WorkbenchUpgrade;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.screens.MenuScreens;
@@ -29,13 +30,11 @@ public class PortableStorageClient implements ClientModInitializer {
         MenuScreens.register(ModScreenHandlers.CRAFTING_WAREHOUSE, CraftingWarehouseScreen::new);
         MenuScreens.register(ModScreenHandlers.BOUND_BARREL, BoundBarrelScreen::new);
 
-        // 注册快捷键
-        openCraftingKey = new KeyMapping(
+        openCraftingKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
                 "key.portablestorage.open_crafting",
                 InputConstants.Type.KEYSYM,
-                InputConstants.UNKNOWN.getValue(), // 默认不设置
-                KeyMapping.Category.register(
-                        Identifier.fromNamespaceAndPath(PortableStorage.MOD_ID, "key_category")));
+                InputConstants.UNKNOWN.getValue(),
+                KeyMapping.Category.register(Identifier.fromNamespaceAndPath(PortableStorage.MOD_ID, "key_category"))));
 
         // 快捷键监听
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
